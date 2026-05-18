ee0f163a1f1862110514bf1cd52983766f4fcdc7
Lee Wang <leeeee@gmail.com>
2026-05-18 16:58:54 -0700

    Revert "removes Header props"
    This reverts commit 1094d1d524af4a39119bae044be1c63ffa3aca37.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return(
     <>
-      <Header />
+      <Header token={token} onSetEmail={setEmail} onSetToken={setToken} />
       {
         token === '' ? (
           <Logon onSetEmail={setEmail} onSetToken={setToken} />
