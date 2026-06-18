72dae7ae88475c5796120caa015964a11bd48271
Lee Wang <leeeee@gmail.com>
2026-06-18 11:45:01 -0700

    Adds linkedin link

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return(
     <footer>
       <hr />
-      &copy; 2026 Lee Wang
+      &copy; 2026 <a href='https://www.linkedin.com/in/liwei-wang-sf/	' target='_blank'>Lee Wang</a>
     </footer>
   )
 }
